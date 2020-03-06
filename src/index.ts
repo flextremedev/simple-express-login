2fa722d70dd16e94a9519f92f0b09ba822f62f45
Pascal Wegner <pascal.wegner@fastleansmart.com>
2020-03-06 12:05:20 +0100

    Setup typescript and linting

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
+  console.log({ req, res });
   res.send("Hello world");
 });
 
