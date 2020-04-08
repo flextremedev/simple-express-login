e3d861ce77b0964efb8f14772c451252e4c9f435
flextremedev <p.wegner1992@gmail.com>
2020-04-08 23:20:16 +0200

    WIP Session setup

--- src/common/express-callback/makeExpressCallback.ts
+++ src/common/express-callback/makeExpressCallback.ts
@@ -6,8 +6,10 @@ export const makeExpressCallback = function makeExpressCallback(
   controller: (httpRequest: HttpRequest) => Promise<HttpResponse>
 ): RequestHandler {
   return async function expressCallback(req: Request, res: Response): Promise<void> {
+    console.log(req.session)
     const httpRequest: HttpRequest = {
       body: req.body,
+      session: req.session,
       query: req.query,
       params: req.params,
       ip: req.ip,
