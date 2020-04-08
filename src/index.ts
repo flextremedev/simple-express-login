4fc3b24493085c19468bf36b8ebe95695c055081
flextremedev <p.wegner1992@gmail.com>
2020-04-08 14:33:23 +0200

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { makeExpressCallback } from "./common/express-callback/makeExpressCallback";
 import bodyParser from "body-parser";
+import cors from "cors";
+import { makeExpressCallback } from "./common/express-callback/makeExpressCallback";
 import { loginController } from "./user-management/login/controllers/loginController";
 import { registrationController } from "./user-management/registration/controllers/registrationController";
 const app = express();
 const port = 8080;
-app.use(bodyParser.json());
+app.use(bodyParser.json(), cors());
 app.post("/login", makeExpressCallback(loginController));
 app.post("/register", makeExpressCallback(registrationController));
 
